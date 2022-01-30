5117b22777c6b759c777f741d506bb26f04bd9c5
PascalF <5677521+PascalFrenz@users.noreply.github.com>
2022-01-30 21:00:19 +0100

    implement uptime-card with mini-card-component

--- mineos-app/src/app/components/mini-cards/uptime-card/uptime-card.component.ts
+++ mineos-app/src/app/components/mini-cards/uptime-card/uptime-card.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnDestroy } from '@angular/core';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { HostHeartbeat } from 'src/app/models/host-heartbeat';
 import { MineosSocketService } from 'src/app/services/mineos-socket.service';
 
 @Component({
   selector: 'app-uptime-card',
-  templateUrl: './uptime-card.component.html',
-  styleUrls: ['./uptime-card.component.scss']
+  template: `
+      <app-mini-card [icon]="faClock"
+                     [value]="(serverUptime$ | async | amDuration:'seconds') ?? 'n/a'"
+                     color="black"
+                     text="Uptime">
+      </app-mini-card>
+  `,
+  styleUrls: []
 })
 export class UptimeCardComponent implements OnDestroy {
-  sub$:Subscription;
-  serverUptime$:BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  faClock = faClock;
+
+  sub$: Subscription;
+  serverUptime$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
   constructor(private mineosSocket: MineosSocketService) {
-    this.sub$ = this.mineosSocket.hostHeartbeat().subscribe((data:HostHeartbeat) => {
+    this.sub$ = this.mineosSocket.hostHeartbeat().subscribe((data: HostHeartbeat) => {
       this.serverUptime$.next(data.uptime);
     })
-   }
-   ngOnDestroy(): void {
-     this.sub$.unsubscribe();
-   }
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
+  }
 }
