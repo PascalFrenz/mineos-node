a2e0b524e4cdefd1805b9aef573b71648d4c452b
PascalF <5677521+PascalFrenz@users.noreply.github.com>
2022-01-30 20:40:53 +0100

    implement dashboard card as component

---
+++ mineos-app/src/app/components/mini-card/mini-card.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input} from '@angular/core';
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+
+@Component({
+  selector: 'app-mini-card',
+  template: `
+    <div class="shadow-md px-6 py-4 bg-white rounded-md grid grid-cols-2">
+      <div class="flex flex-col justify-between">
+        <h1 class="text-4xl font-bold">{{value}}</h1>
+        <span>{{text}}</span>
+      </div>
+      <div class="flex justify-end items-center">
+        <fa-icon [icon]="icon" size="4x" class="text-green-700"></fa-icon>
+      </div>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class MiniCardComponent {
+  @Input() icon!: IconProp;
+  @Input() value!: string | number;
+  @Input() text!: string;
+}
